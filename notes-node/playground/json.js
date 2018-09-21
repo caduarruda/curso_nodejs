77492ec692545ccad544ee05b8dbdf53877c7ad6
Carlos Arruda (Cadu) <ceha1972@gmail.com>
2018-09-20 22:27:50 -0300

    Conteúdo das aulas de 20/09/2018

--- notes-node/playground/json.js
+++ notes-node/playground/json.js
@@ -6,7 +6,25 @@
 // console.log(typeof stringObj);
 // console.log( stringObj );
 
-var personString = '{"name": "Cadu", "age": 45}';
-var person = JSON.parse(personString);
-console.log(typeof person);
-console.log(person);
+// var personString = '{"name": "Cadu", "age": 45}';
+// var person = JSON.parse(personString);
+// console.log(typeof person);
+// console.log(person);
+
+const fs = require( 'fs');
+
+var originalNote = {
+    title: 'Some title',
+    body: 'Some body'
+};
+
+var originalNoteString = JSON.stringify(originalNote);
+fs.writeFileSync('notes.json', originalNoteString);
+
+var noteString = fs.readFileSync('notes.json');
+var note = JSON.parse(noteString);
+
+console.log(typeof note);
+console.log(note.title);
+console.log(note);
+console.log(noteString);
