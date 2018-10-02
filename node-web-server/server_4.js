8466677ce82b836fd2b0226db0be6fdf6fba659b
Carlos Arruda (Cadu) <ceha1972@gmail.com>
2018-10-01 22:34:24 -0300

    Update .gitignore, server.js, and 3 more files...

--- node-web-server/server_4.js
+++ node-web-server/server_4.js
@@ -1,31 +1,14 @@
 const express = require('express');
 const hbs = require('hbs');
-const fs = require('fs');
 
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
-
-app.use((req, res, next) => {
-  var now = new Date().toString();
-  var log = `${now}: ${req.method} ${req.url}`;
-
-  console.log(log);
-  fs.appendFile('server.log', log + '\n', (error) => {
-    if(error) throw error
-  });
-  next();
-});
-
-// app.use((req, res, next) => {
-//   res.render('maintenance.hbs');
-// });
-
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
-  return new Date().getFullYear();
+  return new Date().getFullYear()
 });
 
 hbs.registerHelper('screamIt', (text) => {
