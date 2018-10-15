23298902bc176d67eed9a40c1554a4cc9ecbd6ee
Carlos Arruda (Cadu) <ceha1972@gmail.com>
2018-10-15 18:06:35 -0300

    Atualizações até 15/10/2018

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');    // Importacao do modulo de conexão ao banco de dados Moogoose para a variavel moongose
 var {Todo} = require('./models/todo');        // Importacao do programa (Data Object) todo para a variavel todo
 var {User} = require('./models/user');        // Importacao do programa (Data Object) user para a variavel user
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();    
 // const port = process.env.PORT || 3000;        // Define a porta para o servidor, via parametro externo                      
@@ -99,6 +100,24 @@ app.patch('/todos/:id', (req, res) => {
   }).catch((e) => {
     res.status(400).send();
   })
+});
+
+// Methodo CREATE do CRUD para os Users
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 // Inicializador do server na porta 3000, chamando a funcao anonima de callback e exibindo o log
